6fa0fa71bd485c243a31d980c3caa3e278004dd9
Max <max.dickson2000@gmail.com>
2023-07-03 14:01:04 +1200

    making more update methods and adding a game stats class

--- alien_invasion.py
+++ alien_invasion.py
@@ -113,9 +113,23 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
-        print(len(self.bullets))
+        
+        self._check_bullet_alien_collisions()        
+        
 
 
+    def _check_bullet_alien_collisions(self):
+        """Respond the bullet-alien collisions."""
+        # Remove any bullets and aliens that have collided.
+        collisions = pygame.sprite.groupcollide(
+                self.bullets, self.aliens, True, True)
+        
+        if not self.aliens:
+            # Destroy existing bullets and create new fleet.
+            self.bullets.empty()
+            self._create_fleet()
+        
+        
     def _create_fleet(self):
         """Create the fleet of aliens."""
         # Create an alien and find the number of aliens in a row.
@@ -165,6 +179,10 @@ class AlienInvasion:
         """
         self._check_fleet_edges()
         self.aliens.update()
+        
+        # Look for alien-ship collisions.
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+            print("Ship hit!!!")
 
                              
     def _update_screen(self):
